1b3d95e34f0b56e4d2cdf43feb4418b974beaafa
Maria Marcolina Cardoso <maria.marcolina.eco@gmail.com>
2023-10-17 13:44:23 -0300

    Implementacao correta de graficos, e finalizacao de implementacao usando classes - seguindo a branch de Malu

---
+++ kolmogorov_smirnovClass.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy.stats as st
+
+class KolmogorovSmirnovTest:
+    def __init__(self, data_filename, target_column, alpha):
+        self.alpha = alpha
+        self.data = pd.read_csv(data_filename)
+        self.target_column = target_column
+        self.prepare_data()
+
+    def prepare_data(self):
+        self.frequency_absolute = self.data[self.target_column].value_counts().reset_index()
+        self.frequency_absolute.rename(columns={self.target_column: 'X_i', 'count': 'Fabs'}, inplace=True)
+        self.frequency_absolute = self.frequency_absolute.sort_values(by='X_i', ascending=True).reset_index(drop=True)
+        self.frequency_absolute['Fac'] = np.cumsum(self.frequency_absolute['Fabs'])
+        self.N_samples = sum(self.frequency_absolute['Fabs'])
+        self.frequency_absolute['Fobs'] = self.frequency_absolute['Fac'] / self.N_samples
+        mu = self.data[self.target_column].mean()
+        sd = self.data[self.target_column].std()
+        self.frequency_absolute['Zcalc'] = ((self.frequency_absolute['X_i'] - mu) / sd).round(2)
+
+    def calculate_expected(self):
+        m = len(self.frequency_absolute['Zcalc'])
+        F_esp = []
+
+        for z in range(0, m):
+            value = st.norm.cdf(self.frequency_absolute['Zcalc'][z])
+            F_esp.append(round(value, 4))
+
+        self.frequency_absolute['Fesp'] = F_esp
+        self.frequency_absolute['Exp(i)_Obs(i)'] = abs(np.array(F_esp) - self.frequency_absolute['Fobs'])
+
+        Fesp_obs2 = [round(self.frequency_absolute['Fesp'][0], 4)]
+        for i in range(1, len(F_esp)):
+            value = F_esp[i] - self.frequency_absolute['Fobs'][i - 1]
+            Fesp_obs2.append(value)
+
+        self.frequency_absolute['Exp(i)_Obs(i-1)'] = Fesp_obs2
+
+    def run_test(self):
+        max1 = max(self.frequency_absolute['Exp(i)_Obs(i)'])
+        max2 = max(self.frequency_absolute['Exp(i)_Obs(i-1)'])
+        D_calc = max(max1, max2)
+        # Achar valor critico
+        criticalV = pd.read_csv("kolmogorovCriticalvalues.csv")
+        row_index = criticalV[criticalV['n'] == self.N_samples].index[0]
+        
+        if self.N_samples <= 35:
+            Dcrit = criticalV.loc[row_index][str(self.alpha)]
+        else:
+            match self.alpha:
+                case 0.2:
+                    Dcrit = 1.07/math.sqrt(self.N_samples)
+                case 0.15:
+                    Dcrit = 1.14/math.sqrt(self.N_samples)
+                case 0.1:
+                    Dcrit = 1.22/math.sqrt(self.N_samples)
+                case 0.05:
+                    Dcrit = 1.36/math.sqrt(self.N_samples)
+                case 0.01:
+                    Dcrit = 1.63/math.sqrt(self.N_samples)
+
+
+        print("\n----------- Tabela: Kolmogorov-Smirnov -----------")
+        print(self.frequency_absolute)
+
+        print("\nDcalculado:", round(D_calc, 3))
+        print("DCritico:", round(Dcrit, 3))
+
+        if D_calc > Dcrit:
+            print(" --> A variável NAO Segue uma Distribuicao Normal <--")
+        else:
+            print(" --> A variável Segue uma Distribuicao Normal <--")
+
+    def plot_histograms(self):
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+        ax1.hist(self.data[self.target_column], bins=6, edgecolor='black') #self.frequency_absolute['Fabs']
+        variable_name = self.target_column
+        ax1.set_xlabel(f'{variable_name}')
+        ax1.set_title('Histogram')
+        ax2.scatter(self.frequency_absolute['Fesp'],self.frequency_absolute['Fobs'])
+        ax2.set_title('Esperado x Observado')
+        ax2.set_ylabel("Frequencia Observada")
+        ax2.set_xlabel("Frequencia Esperada")
+        
+        # Define the parameters of the model (slope and intercept)
+        slope = 1  # Change this to your desired slope
+        intercept = 0  # Change this to your desired intercept
+
+        # Add the line of the model
+        x_model = np.linspace(0, 1, 100)  # Create x-values for the line
+        y_model = slope * x_model + intercept  # Calculate y-values based on the model
+        plt.plot(x_model, y_model, linestyle='-', color='red', label=f'Model: y = {slope}x + {intercept}')
+        plt.show()
+
+if __name__ == "__main__":
+    ks = KolmogorovSmirnovTest("maquinaAgric2.csv", "Faturamento", 0.05)
+    ks.calculate_expected()
+    ks.run_test()
+    ks.plot_histograms()
+    
+    
