9004fe991ec0c0a500d25c25bc56f795de03c698
Maria Luiza Leite dos Santos <yukiluizamaria@gmail.com>
2023-10-29 15:54:36 -0300

    Quais são os casos t-student pedidos -> anotado

---
+++ t-student/t-student.py
@@ -0,0 +1,8 @@
+# t-student: geralmente amostras < 30
+# Teste t de Student quando o desvio-padrão populacional ( σ) não for conhecido
+
+# TESTE t DE STUDENT PARA COMPARAÇÃO DE DUAS MÉDIAS POPULACIONAIS A PARTIR DE DUAS AMOSTRAS ALEATÓRIAS INDEPENDENTES
+        # Caso 1: variâncias populacionais diferentes
+        # Caso 2: Variâncias populacionais homogêneas
+
+# TESTE t DE STUDENT PARA COMPARAÇÃO DE DUAS MÉDIAS POPULACIONAIS A PARTIR DE DUAS AMOSTRAS ALEATÓRIAS EMPARELHADAS
